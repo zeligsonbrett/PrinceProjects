a7fcc8dde517b9aa145848134ed11f04207853fe
zeligsongit <zeligson@princeton.edu>
2022-03-24 01:16:26 -0400

    Article 2

--- script.js
+++ script.js
@@ -12,6 +12,18 @@ function expand() {
   }
 }
 
+$(document).scroll(function() {
+
+  myID = document.getElementById("button");
+
+  var myScrollFunc = function () {
+      var y = window.scrollY;
+      if (y >= 100) {
+          myID.className = "button show"
+      } else {
+          myID.className = "button hide"
+      }
+  };
 
 
 
